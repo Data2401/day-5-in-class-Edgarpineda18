601eb384eb2e2aa20df024a062ab2de763283efd
Edgar Pineda <edgarnursepineda@gmail.com>
2025-02-05 16:10:54 -0600

    Commiting the correct file

--- InClassDay5.R
+++ InClassDay5.R
@@ -13,55 +13,67 @@
 ### PART ONE - Review of Objects ####
 
 # Assign your name to a variable called `my_name`
-
+my_name <- "Edgar Pineda"
 # Assign the number of pets you have to a variable called `num_pets`
-
+num_pets <- 0
 # Assign the title of your favorite book as a variable called `favorite_book`
-
+favorite_book <- 'shipbreaker'
 # Create a variable called `radius`, which is your favorite number
-
+radius <- 94
 # Create a variable called `area`, which is the area of a circle with that radius.
 # Use the variable `radius` to calculate this. 
 # Use the built-in constant `pi` for pi.
-
+area <- pi * radius^2
 
 # Create a boolean variable `too_big` that is set to TRUE if the area is larger than 100
 # Use a relationship operator to do this  (don't just set it to "TRUE")
 # Think: how do you check if it's larger than 10? Assign that to your new variable name. 
-
+too_big <- area >100
 
 # Display the variable `my_name`
 # Display the variable `area` 
 # Display the variable `too_big`
-
-
+my_name
+area
+too_big
 ### PART TWO ###
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name, using the variable you made above. (use the `nchar()` function)
-
+my_name2 <- nchar(my_name)
 # Print the number of letters in your name
-
+print(my_name2)
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
-
+now_doing <- paste('Edgar', "is programming!")
 # Make the `now_doing` variable upper case
-
+toupper(now_doing)
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
-
+fav_1 <- 94
+fav_2 <- 96
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-
+fav_1/sqrt(201)
+fav_2/sqrt(201)
+print(fav_1)
+print(fav_2)
+fav_1 <- 6.630249
+fav_2 <- 6.771318
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw_sum <- sum(fav_1, fav_2)
+print(raw_sum)
 # Create a variable `round_sum` that is the `raw_sum` rounded to 2 decimal places.
 # Use the `round()` function.
-
+round_sum <- round(raw_sum, digits = 2)
+print(round_sum)
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 2 decimal places
-
+round_1 <- round(fav_1, digits = 2)
+round_2 <- round(fav_2, digits = 2)
 # Create a variable `sum_round` that is the sum of the rounded values
-
+sum_round <- sum(round_1,round_2)
 # Print out both variables and compare. Are they the same?
+print(raw_sum)
+print(sum_round)
